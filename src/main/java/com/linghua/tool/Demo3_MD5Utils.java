d2e3eda92d914202f3ee7cf49e8b57bc940a5c2d
liyunfei <1250429552@qq.com>
2018-07-23 15:26:12 +0800

    提交全部文件

---
+++ src/main/java/com/linghua/tool/Demo3_MD5Utils.java
@@ -0,0 +1,64 @@
+package com.linghua.tool;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+/**
+ * @author yunfei.li
+ * @project maoyan-show-m-web
+ * @package com.maoyan.show.utils
+ * @date 17/7/19
+ */
+public class Demo3_MD5Utils {
+
+    public static void main(String[] args){
+        System.out.println(string2MD5("http://wiki.maoyan.com/pages/viewpage.action?pageId=5801903"));
+        System.out.println(getMD5("http://wiki.maoyan.com/pages/viewpage.action?pageId=5801903"));
+    }
+
+    /***
+     * MD5加码 生成32位md5码
+     */
+    public static String string2MD5(String inStr){
+        MessageDigest md5 = null;
+        try{
+            md5 = MessageDigest.getInstance("MD5");
+        }catch (Exception e){
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
+        }
+        char[] charArray = inStr.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++)
+            byteArray[i] = (byte) charArray[i];
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < md5Bytes.length; i++){
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16)
+                hexValue.append("0");
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+
+    }
+
+    public static String getMD5(String str) {
+        try {
+            // 生成一个MD5加密计算摘要
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(str.getBytes());
+            // digest()最后确定返回md5 hash值，返回值为8为字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+            return new BigInteger(1, md.digest()).toString(16);
+        } catch (Exception e) {
+            return StringUtils.EMPTY;
+        }
+    }
+
+}
